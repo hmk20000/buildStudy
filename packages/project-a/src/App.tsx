d54773036ce6919d1efd7b4c897551a2478fe4c9
codeming <hmk2000015@gmail.com>
2025-01-03 21:39:09 +0900

    workspace init

---
+++ packages/project-a/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import ProjectBApp from '../../project-b/src/App'
+
+function App() {
+
+  return (
+    <>
+      <ProjectBApp />
+      ddd
+    </>
+  )
+}
+
+export default App
---
+++ packages/project-b/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      test111211
+    </>
+  )
+}
+
+export default App
