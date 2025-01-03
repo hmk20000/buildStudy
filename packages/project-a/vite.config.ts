902acbc73a7c19768ccae0a655aa78e2a2c41444
codeming <hmk2000015@gmail.com>
2025-01-03 21:45:37 +0900

    add action

--- packages/project-a/vite.config.ts
+++ packages/project-a/vite.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/project-a/', // 프로젝트 이름과 일치하도록 설정
+});
